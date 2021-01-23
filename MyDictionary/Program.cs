457b6c613055a52188bb459df9baba2dcc3760a0
Çağrı Tanyıldızı <cgrtnyldz@hotmail.com>
2021-01-23 14:41:47 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ogrenciler = new List<string>();
+            ogrenciler.Add("Çağrı");
+            ogrenciler.Add("Jale");
+            ogrenciler.Add("Engin");
+            ogrenciler.Add("Murat");
+            ogrenciler.Add("Ali");
+            Console.WriteLine(ogrenciler.Count);
+
+
+            MyDictionary<string>ogrenciler1=new MyDictionary<string>();
+            ogrenciler1.Add("Rana");
+            ogrenciler1.Add("Ferhat");
+            ogrenciler1.Add("Yunus");
+            ogrenciler1.Add("Berkay");
+            ogrenciler1.Add("Kenan");
+            ogrenciler1.Add("Ahmet");
+            Console.WriteLine(ogrenciler1.Count);
+
+        }
+    }
+}
